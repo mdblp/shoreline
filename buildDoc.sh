4c77d95cf4a24bf676e0306dfaa90542b5a09be5
Fabrice Sougey - Diabeloop <fabrice@diabeloop.fr>
2020-07-07 17:06:19 +0200

    Remove dblp prefix in openapi doc name

--- buildDoc.sh
+++ buildDoc.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -eux
+#!/bin/bash -eux
 # Generate OpenAPI documentation
 GOPATH=${GOPATH:-~/go}
 echo "Using GOPATH: ${GOPATH}"
@@ -14,7 +14,7 @@ $GOPATH/bin/swag init --generalInfo shoreline.go --output docs
 # When tag is present, openapi doc is renamed before being deployed to S3
 if [ -n "${TRAVIS_TAG:-}" ]; then
     APP="${TRAVIS_REPO_SLUG#*/}"
-    APP_TAG="${APP}-${TRAVIS_TAG}"
+    APP_TAG="${APP}-${TRAVIS_TAG/dblp./}"
     mkdir docs/openapi
     mv docs/swagger.json docs/openapi/${APP_TAG}-swagger.json
 fi
