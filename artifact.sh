f5960c9c35ef8d0ae794fb41fab224e27e209aa0
Fabrice Sougey - Diabeloop <fabrice@diabeloop.fr>
2020-10-20 11:50:38 +0200

    Test

--- artifact.sh
+++ artifact.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
-wget -q -O artifact_go.sh 'https://raw.githubusercontent.com/mdblp/tools/feature/review_security_scan/artifact/artifact_go.sh'
+wget -q -O artifact_go.sh 'https://raw.githubusercontent.com/mdblp/tools/feature/add_platform/artifact/artifact_go.sh'
 chmod +x artifact_go.sh
 
 . ./version.sh
