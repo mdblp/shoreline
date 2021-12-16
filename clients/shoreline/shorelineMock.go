e9d29e92b917663d17cf7a16da38d09a4993c59e
Yann Blanc <yann.blanc@diabeloop.fr>
2021-12-16 16:58:45 +0100

    fix test mock data

--- clients/shoreline/shorelineMock.go
+++ clients/shoreline/shorelineMock.go
@@ -68,7 +68,7 @@ func (client *ShorelineMockClient) GetUser(userID, token string) (*schema.UserDa
 
 func (client *ShorelineMockClient) GetUnverifiedUsers(token string) ([]schema.UserData, error) {
 	return []schema.UserData{
-		{UserID: "randomId", Username: "From Mock", Emails: []string{"randomEmails"}, PasswordExists: false, Roles: []string{"patient"}, TermsAccepted: "false"},
+		{UserID: "randomId", Username: "From Mock", Emails: []string{"randomEmails"}, PasswordExists: false, Roles: []string{"patient"}, EmailVerified: false},
 	}, nil
 }
 
