0aba7f95c97d92bb7c0d47c49dfeb7d524bb9a4e
ydris <ydris@diabeloop.fr>
2022-06-09 09:54:47 +0200

    ylp-1523 replicate user updates on auth0
    required new env vars:
    * AUTH0_URL
    * AUTH0_SECRET
    * AUTH0_CLIENT_ID

--- auth0/client.go
+++ auth0/client.go
@@ -1,6 +1,7 @@
 package auth0
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 	"net/url"
@@ -16,21 +17,27 @@ import (
 )
 
 type userMetaData struct {
-	Role string `json:"role"`
+	Role string `json:"role,omitempty"`
 }
 
+// user structure used by Auth0 management api
 type auth0User struct {
-	Email         string       `json:"email"`
-	EmailVerified bool         `json:"email_verified"`
-	UserId        string       `json:"user_id"`
-	Metadata      userMetaData `json:"user_metadata"`
+	Email         string        `json:"email,omitempty"`
+	EmailVerified bool          `json:"email_verified,omitempty"`
+	UserId        string        `json:"user_id,omitempty"`
+	Metadata      *userMetaData `json:"user_metadata,omitempty"`
+	Password      string        `json:"password,omitempty"`
+	Connection    string        `json:"connection,omitempty"`
 }
 
+// http body content received from auth0 when requesting an access token (/oauth/token)
 type auth0Token struct {
 	AccessToken string `json:"access_token"`
 	ExpiresIn   int    `json:"expires_in"`
 }
 
+// Auth0 api client object
+// Used to communicate with Auth0 management api using machine credentials
 type Auth0Client struct {
 	// Client id, secret and url are required to establish a connection to Auth0 and get tokens
 	clientId        string
@@ -46,9 +53,11 @@ type Auth0Client struct {
 	audience        string
 }
 
+// Create a new client from environment variables
+// requires AUTH0_URL, AUTH0_SECRET and AUTH0_CLIENT_ID to be set
 func NewAuth0Client(logger *log.Logger) *Auth0Client {
 	auth0Url := os.Getenv("AUTH0_URL")
-	auth0Secret := os.Getenv("AUTH0_Secret")
+	auth0Secret := os.Getenv("AUTH0_SECRET")
 	auth0ClientId := os.Getenv("AUTH0_CLIENT_ID")
 	if auth0Url == "" || auth0Secret == "" {
 		logger.Fatal("Auth0 configuration not provided")
@@ -72,6 +81,7 @@ func NewAuth0Client(logger *log.Logger) *Auth0Client {
 	}
 }
 
+// Get an auth token and initiate the token refresh loop
 func (client *Auth0Client) Start() error {
 	var err error
 	if client.secret == "" || client.clientId == "" {
@@ -88,6 +98,7 @@ func (client *Auth0Client) Start() error {
 
 func (client *Auth0Client) serverLoginLoop(launchRefreshTokenLoop bool) {
 	var attempts int64
+	waitPeriod, _ := time.ParseDuration("5s")
 	client.mut.Lock()
 	if client.acquiringToken {
 		client.mut.Unlock()
@@ -96,7 +107,7 @@ func (client *Auth0Client) serverLoginLoop(launchRefreshTokenLoop bool) {
 	client.acquiringToken = true
 	client.mut.Unlock()
 	for {
-		timer := time.After(client.refreshInterval)
+		timer := time.After(waitPeriod)
 		select {
 		case twoWay := <-client.closed:
 			twoWay <- true
@@ -140,6 +151,8 @@ func (client *Auth0Client) refreshTokenLoop() {
 		}
 	}
 }
+
+//
 func (client *Auth0Client) Close() {
 	twoWay := make(chan bool)
 	client.closed <- twoWay
@@ -155,6 +168,7 @@ func (client *Auth0Client) Close() {
 	client.token = ""
 }
 
+// Get an access token from Auth0
 func (client *Auth0Client) serverLogin() error {
 
 	var loginResult auth0Token
@@ -188,6 +202,7 @@ func (client *Auth0Client) serverLogin() error {
 	return nil
 }
 
+// Get user info from Auth0 using the email as key
 func (client *Auth0Client) GetUser(email string) (*schema.UserData, error) {
 	params := url.Values{}
 	params.Add("q", "email:\""+email+"\"")
@@ -222,6 +237,63 @@ func (client *Auth0Client) GetUser(email string) (*schema.UserData, error) {
 	return user, nil
 }
 
-func (client *Auth0Client) UpdateUser(email string) bool {
-	return true
+// Get user info from Auth0 using its user id as key
+func (client *Auth0Client) GetUserById(id string) (*schema.UserData, error) {
+	url, _ := url.Parse(client.userUrl + "/auth0|" + id)
+	req, _ := http.NewRequest("GET", url.String(), nil)
+	req.Header.Add("authorization", "Bearer "+client.token)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failure to get a user")
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, errors.New("Error while requesting Auth0: " + res.Status)
+	}
+	var users auth0User
+	if err := json.NewDecoder(res.Body).Decode(&users); err != nil {
+		return nil, err
+	}
+	user := &schema.UserData{
+		UserID:        users.UserId,
+		Username:      users.Email,
+		EmailVerified: users.EmailVerified,
+		Emails:        []string{users.Email},
+		Roles:         []string{users.Metadata.Role},
+	}
+	return user, nil
+}
+
+// Update a user on Auth0
+// Only the Email and Password can be udated for now
+// Updating both email and password at the same time will throw an error (not accepted by Auth0)
+func (client *Auth0Client) UpdateUser(id string, user *schema.UserUpdate) error {
+	updUser := auth0User{}
+	updUser.Connection = "Username-Password-Authentication"
+	if user.Username != nil && *user.Username != "" {
+		updUser.Email = *user.Username
+	}
+	if user.Password != nil && *user.Password != "" {
+		updUser.Password = *user.Password
+	}
+	if user.Password == nil && user.Username == nil {
+		return nil
+	}
+	jsonUser, _ := json.Marshal(updUser)
+	url, _ := url.Parse(client.userUrl + "/auth0|" + id)
+	req, _ := http.NewRequest("PATCH", url.String(), bytes.NewBuffer(jsonUser))
+	req.Header.Add("authorization", "Bearer "+client.token)
+	req.Header.Add("content-type", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return errors.New("Error while updating Auth0: " + err.Error())
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return errors.New("Error while updating Auth0: " + res.Status)
+	}
+	return nil
 }
--- user/api.go
+++ user/api.go
@@ -24,6 +24,7 @@ import (
 
 	"github.com/mdblp/go-common/clients/status"
 	"github.com/mdblp/shoreline/auth0"
+	"github.com/mdblp/shoreline/schema"
 	"github.com/mdblp/shoreline/token"
 	"github.com/mdblp/shoreline/user/middlewares"
 
@@ -539,8 +540,8 @@ func (a *Api) CreateUser(res http.ResponseWriter, req *http.Request) {
 func (a *Api) UpdateUser(res http.ResponseWriter, req *http.Request, vars map[string]string) {
 	log := middlewares.GetLogReq(req)
 	log.Info("processing a update user request")
-
 	sessionToken := sanitizeSessionToken(req)
+	var auth0User = &schema.UserUpdate{}
 	if tokenData, err := a.authenticateSessionToken(req.Context(), sessionToken); err != nil {
 		a.sendError(res, http.StatusUnauthorized, STATUS_UNAUTHORIZED, log, err)
 
@@ -604,10 +605,12 @@ func (a *Api) UpdateUser(res http.ResponseWriter, req *http.Request, vars map[st
 			dupCheck := &User{}
 			if updateUserDetails.Username != nil {
 				updatedUser.Username = *updateUserDetails.Username
+				auth0User.Username = updateUserDetails.Username
 				dupCheck.Username = updatedUser.Username
 			}
 			if updateUserDetails.Emails != nil {
 				updatedUser.Emails = updateUserDetails.Emails
+				auth0User.Emails = &updateUserDetails.Emails
 				dupCheck.Emails = updatedUser.Emails
 			}
 
@@ -629,6 +632,7 @@ func (a *Api) UpdateUser(res http.ResponseWriter, req *http.Request, vars map[st
 				a.sendError(res, http.StatusInternalServerError, STATUS_ERR_UPDATING_USR, log, err)
 				return
 			}
+			auth0User.Password = updateUserDetails.Password
 		}
 
 		if updateUserDetails.Roles != nil {
@@ -646,6 +650,17 @@ func (a *Api) UpdateUser(res http.ResponseWriter, req *http.Request, vars map[st
 		if err := a.Store.UpsertUser(req.Context(), updatedUser); err != nil {
 			a.sendError(res, http.StatusInternalServerError, STATUS_ERR_UPDATING_USR, log, err)
 		} else {
+			if a.auth0Client != nil {
+				if usr, err := a.auth0Client.GetUserById(updatedUser.Id); err == nil && usr != nil {
+					if usr.Username == updatedUser.Username {
+						// no need to update with the same value
+						auth0User.Username = nil
+					}
+					if err := a.auth0Client.UpdateUser(updatedUser.Id, auth0User); err != nil {
+						a.logger.Error("Impossible to update user on Auth0: ", err)
+					}
+				}
+			}
 			a.logAudit(req, tokenData, "update request succedeed for username:%s, and is a clinician one{%t}", updatedUser.Username, updatedUser.IsClinic())
 			log.Infof("update request succedeed for username:%s, and is a clinician one{%t}", updatedUser.Username, updatedUser.IsClinic())
 			a.sendUser(res, updatedUser, tokenData.IsServer)
